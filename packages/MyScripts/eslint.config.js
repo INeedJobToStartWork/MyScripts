b1153a81e823261817f4314f408817d35088f87b
INeedJobToStartWork <ineedjobtostartwork@gmail.com>
2024-07-16 16:49:54 +0200

    ⚙️  Chore(myscripts): prepare enviroment (config packages, scripts)

---
+++ packages/MyScripts/eslint.config.js
@@ -0,0 +1,23 @@
+import ineedj from "@ineedj/eslintrc";
+
+export default ineedj({
+	formatters: {
+		json: false,
+		stylistic: false,
+		stylisticJSX: false,
+		stylisticTS: false,
+		perfectionistSorters: false
+	},
+	syntax: {
+		eslint: true,
+		jsx: false,
+		next: false,
+		node: true,
+		react: false,
+		storybook: false,
+		tailwindcss: false,
+		typescript: true,
+		toml: false,
+		yaml: false
+	}
+});
